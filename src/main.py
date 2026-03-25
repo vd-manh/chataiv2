9b0bfeea1318efc19609ad6b6287596b7482f367
VuDuyManh2020603281 <110422164+vd-manh@users.noreply.github.com>
2026-03-25 18:28:05 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import uuid
 import shutil
 from datetime import datetime
 import streamlit as st
 import pysqlite3
-import sys
-sys.modules["sqlite3"] = pysqlite3
+
+
 # --- CẤU HÌNH TRANG (PHẢI ĐẶT ĐẦU TIÊN) ---
 st.set_page_config(
     page_title="VMVM AI", 
